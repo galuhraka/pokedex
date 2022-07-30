a7a7761e124ae1f6753b343c934ae6eb67310571
tarripewe <tarripewe01@gmail.com>
2022-07-30 14:26:18 +0700

    Auth Logout

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,14 +1,12 @@
 import React from "react";
-import Topbar from "../components/topbar/Topbar";
-import Pokelist from "../components/pokelist/Pokelist";
 import Bottombar from "../components/bottombar/Bottombar";
 import Header from "../components/Header/Header";
+import Pokelist from "../components/pokelist/Pokelist";
 
 const Homepage = () => {
   return (
     <>
     <Header />
-      {/* <Topbar /> */}
       <Pokelist />
       <Bottombar />
     </>
